4abdb0304a2ca8ea35cffab3429bc470b430516a
Nathan Sherburn <nathansherburn@gmail.com>
2016-03-30 04:19:48 +1100

    Add cool stuff

--- client/index.js
+++ client/index.js
@@ -1,6 +1,29 @@
 var myApp = angular.module('myApp',[])
 
-myApp.controller('MyController', function ($scope) {
+myApp.service('HistoryService', function ($http) {
+	var baseUrl = "http://localhost:8080/"
+
+	this.saveWord = function (newWord) {
+		var url = baseUrl + "saveCurrent"
+		return $http.post(url, {word: newWord})
+	}
+
+})
+
+myApp.controller('MyController', function ($scope, HistoryService) {
 	$scope.newWord = 'cat'
+
+	$scope.saveThisWord = function () {
+		HistoryService.saveWord( $scope.newWord )
+		.then(saveSuccess, error)
+	}
+
+	function saveSuccess (json) {
+		console.log(json)
+	} 
+
+	function error (err) {
+		console.log(err)
+	}
 })
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+var Nedb = require('nedb')
+
+var database = new Nedb({filename:'./data/data.db', autoload: true})
 
 app.use(express.static('../client'))
+
+app.use(bodyParser.json())
+
+app.post('/saveCurrent', function (req, res) {
+	var data = { word: req.body.word, date: Date.now() }
+
+	var done = function () {
+		console.log("I just wrote something to the database!!!")
+		res.end("done")
+	}
+
+	database.insert(data, done)
+})
 
 app.listen(8080)
